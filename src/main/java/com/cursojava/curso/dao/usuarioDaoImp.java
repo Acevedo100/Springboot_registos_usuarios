3feb0cf55ef59b7934e4b0ee2a4d56f43a9283b3
Acevedo100 <75899140+Acevedo100@users.noreply.github.com>
2021-10-23 12:43:46 -0300

    Se agrega el proyecto

---
+++ src/main/java/com/cursojava/curso/dao/usuarioDaoImp.java
@@ -0,0 +1,58 @@
+package com.cursojava.curso.dao;
+
+import com.cursojava.curso.models.Usuario;
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+@Transactional
+public class usuarioDaoImp implements UsuarioDao {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Override
+    @Transactional
+    public List<Usuario> getUsuarios() {
+        String query = "FROM Usuario";
+        return entityManager.createQuery(query).getResultList();
+    }
+
+    @Override
+    public void Eliminar(Long id) {
+        Usuario usuario = entityManager.find(Usuario.class,id);
+        entityManager.remove(usuario);
+    }
+
+    @Override
+    public void registrar(Usuario usuario) {
+        entityManager.merge(usuario);
+    }
+
+
+
+    public boolean verificarCredenciales(Usuario usuario){
+        String query = "FROM Usuario WHERE email = :email";
+        List <Usuario> lista = entityManager.createQuery(query)
+                .setParameter("email",usuario.getEmail())
+                .getResultList();
+
+        if (lista.isEmpty()){
+           return false;
+        }
+
+        String passwordHashed = lista.get(0).getPassword();
+
+        Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
+
+        // Revisar credenciales ya que no esta regresando lo esperado
+        //return argon2.verify(passwordHashed, usuario.getPassword());
+        return true;
+    }
+}
